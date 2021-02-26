56ed9d8b7a83f5f15f3987af1fce10ea5f3cbe22
eed2e <eed.esteban@gmail.com>
2021-02-26 17:55:32 -0600

    base de datos

---
+++ elms.sql
@@ -0,0 +1,221 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 27-02-2021 a las 00:54:45
+-- Versión del servidor: 10.4.8-MariaDB
+-- Versión de PHP: 7.1.32
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `elms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `UserName` varchar(100) NOT NULL,
+  `Password` varchar(100) NOT NULL,
+  `updationDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `admin`
+--
+
+INSERT INTO `admin` (`id`, `UserName`, `Password`, `updationDate`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', '2021-02-26 22:28:29'),
+(2, 'carlos', 'dc599a9972fde3045dab59dbd1ae170b', '2016-04-21 05:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbldepartments`
+--
+
+CREATE TABLE `tbldepartments` (
+  `id` int(11) NOT NULL,
+  `DepartmentName` varchar(150) DEFAULT NULL,
+  `DepartmentShortName` varchar(100) NOT NULL,
+  `DepartmentCode` varchar(50) DEFAULT NULL,
+  `CreationDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `tbldepartments`
+--
+
+INSERT INTO `tbldepartments` (`id`, `DepartmentName`, `DepartmentShortName`, `DepartmentCode`, `CreationDate`) VALUES
+(5, 'Redes', 'red', '032034', '2018-06-26 17:15:19');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tblemployees`
+--
+
+CREATE TABLE `tblemployees` (
+  `id` int(11) NOT NULL,
+  `EmpId` varchar(100) NOT NULL,
+  `FirstName` varchar(150) NOT NULL,
+  `LastName` varchar(150) NOT NULL,
+  `EmailId` varchar(200) NOT NULL,
+  `Password` varchar(180) NOT NULL,
+  `Gender` varchar(100) NOT NULL,
+  `Dob` varchar(100) NOT NULL,
+  `Department` varchar(255) NOT NULL,
+  `Address` varchar(255) NOT NULL,
+  `City` varchar(200) NOT NULL,
+  `Country` varchar(150) NOT NULL,
+  `Phonenumber` char(11) NOT NULL,
+  `Status` int(1) NOT NULL,
+  `RegDate` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `tblemployees`
+--
+
+INSERT INTO `tblemployees` (`id`, `EmpId`, `FirstName`, `LastName`, `EmailId`, `Password`, `Gender`, `Dob`, `Department`, `Address`, `City`, `Country`, `Phonenumber`, `Status`, `RegDate`) VALUES
+(5, '20202', 'sdfghsdgfbhxfsbg', ' c xcbv', '1111@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 'Female', '20 October, 1994', 'dep informatica', 'asc', 'xcdvz', 'xczvz', '4468527931', 1, '2021-02-26 22:19:25');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tblleaves`
+--
+
+CREATE TABLE `tblleaves` (
+  `id` int(11) NOT NULL,
+  `LeaveType` varchar(110) NOT NULL,
+  `ToDate` varchar(120) NOT NULL,
+  `FromDate` varchar(120) NOT NULL,
+  `Description` mediumtext NOT NULL,
+  `PostingDate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `AdminRemark` mediumtext DEFAULT NULL,
+  `AdminRemarkDate` varchar(120) DEFAULT NULL,
+  `Status` int(1) NOT NULL,
+  `IsRead` int(1) NOT NULL,
+  `empid` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `tblleaves`
+--
+
+INSERT INTO `tblleaves` (`id`, `LeaveType`, `ToDate`, `FromDate`, `Description`, `PostingDate`, `AdminRemark`, `AdminRemarkDate`, `Status`, `IsRead`, `empid`) VALUES
+(7, 'Casual Leave', '30/11/2017', '29/10/2017', 'test description test descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest description', '2017-11-19 13:11:21', 'Lorem Ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.\r\n', '2017-12-02 23:26:27 ', 2, 1, 1),
+(8, 'Medical Leave test', '21/10/2017', '25/10/2017', 'test description test descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest description', '2017-11-20 11:14:14', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '2017-12-02 23:24:39 ', 1, 1, 1),
+(9, 'Medical Leave test', '08/12/2017', '12/12/2017', 'Lorem Ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.\r\n', '2017-12-02 18:26:01', 'esta aprobada esta solcitud del empleado', '2018-06-26 22:47:13 ', 1, 1, 2),
+(10, 'Restricted Holiday(RH)', '25/12/2017', '25/12/2017', 'Lorem Ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.', '2017-12-03 08:29:07', 'Lorem Ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.', '2017-12-03 14:06:12 ', 1, 1, 1),
+(11, 'vacaciones empleados', '01/06/2018', '28/06/2018', 'permiso vaciones', '2018-06-26 17:19:43', 'aprobada por vacaciones', '2018-06-26 22:50:35 ', 1, 1, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tblleavetype`
+--
+
+CREATE TABLE `tblleavetype` (
+  `id` int(11) NOT NULL,
+  `LeaveType` varchar(200) DEFAULT NULL,
+  `Description` mediumtext DEFAULT NULL,
+  `CreationDate` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `tblleavetype`
+--
+
+INSERT INTO `tblleavetype` (`id`, `LeaveType`, `Description`, `CreationDate`) VALUES
+(5, 'vacaciones empleados', 'vacaciones empleados', '2018-06-26 17:18:05');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `tbldepartments`
+--
+ALTER TABLE `tbldepartments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `tblemployees`
+--
+ALTER TABLE `tblemployees`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `tblleaves`
+--
+ALTER TABLE `tblleaves`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `UserEmail` (`empid`);
+
+--
+-- Indices de la tabla `tblleavetype`
+--
+ALTER TABLE `tblleavetype`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `tbldepartments`
+--
+ALTER TABLE `tbldepartments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `tblemployees`
+--
+ALTER TABLE `tblemployees`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `tblleaves`
+--
+ALTER TABLE `tblleaves`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT de la tabla `tblleavetype`
+--
+ALTER TABLE `tblleavetype`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
